ee8fb1d93afea6904cc53fa0e4bcda5a77149520
Theme97 <theme97@gmail.com>
2013-01-22 14:26:01 -0800

    import lwjgl and slick and make a blank window

--- src/com/theme97/GauchoSpace/GauchoSpace.java
+++ src/com/theme97/GauchoSpace/GauchoSpace.java
@@ -3,18 +3,43 @@
  */
 package com.theme97.GauchoSpace;
 
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+
 /**
  * @author Michael Fong, Bohan Lin, Daniel Ly, Spencer Pao, Daniel Reinhart
  *
  */
-public class GauchoSpace {
-
+public class GauchoSpace extends BasicGame {
+	
+	public GauchoSpace() {
+		super("Gaucho Space");
+	}
+	
+	@Override
+	public void init(GameContainer gc) throws SlickException {
+		
+	}
+	
+	@Override
+	public void update(GameContainer gc, int delta) throws SlickException {
+		
+	}
+	
+	public void render(GameContainer gc, Graphics g) throws SlickException {
+		
+	}
+	
 	/**
 	 * @param args Unused.
 	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws SlickException {
+		AppGameContainer app = new AppGameContainer(new GauchoSpace());
+		app.setDisplayMode(800,  600,  false);
+		app.start();
 	}
 
 }
