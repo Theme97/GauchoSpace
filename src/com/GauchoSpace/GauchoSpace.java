aa4ecf9fe5cd907664c06ffc01810ec403da63b6
Theme97 <theme97@gmail.com>
2013-01-22 20:38:19 -0800

    remove unused imports

--- src/com/GauchoSpace/GauchoSpace.java
+++ src/com/GauchoSpace/GauchoSpace.java
@@ -5,8 +5,6 @@ package com.GauchoSpace;
 
 import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.GameContainer;
-import org.newdawn.slick.Graphics;
-import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.StateBasedGame;
 
